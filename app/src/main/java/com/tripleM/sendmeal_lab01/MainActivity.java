6c0690d95b4bf17d3b6e214da501070f7097a1da
micapierotti <micapierotti@live.com.ar>
2020-09-24 09:26:28 -0300

    lab01 24/09 09:26 a.m. - mica

--- app/src/main/java/com/tripleM/sendmeal_lab01/MainActivity.java
+++ app/src/main/java/com/tripleM/sendmeal_lab01/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+//
         btnRegistrar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
