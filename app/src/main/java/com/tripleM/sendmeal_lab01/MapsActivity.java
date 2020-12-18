f0295e5a425073b7604334dd8fda41c1cc654467
micapierotti <micapierotti@live.com.ar>
2020-12-18 18:32:44 -0300

    commit lab 05 -  mapas - 18/12 18:27hs

--- app/src/main/java/com/tripleM/sendmeal_lab01/MapsActivity.java
+++ app/src/main/java/com/tripleM/sendmeal_lab01/MapsActivity.java
@@ -3,38 +3,35 @@ package com.tripleM.sendmeal_lab01;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.android.SphericalUtil;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -42,12 +39,14 @@ import java.util.Random;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LatLng ubicacionPedido;
+    private LatLng ubicacionPedido, ubicacionLocal;
     private Button confirmar;
     private TextView ubicacion;
     private Location ubicacionActual;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 101;
+    private  MarkerOptions markerLocal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,9 +137,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         .title("Destino del pedido")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
                 mMap.clear();
+                //mMap.addMarker(markerLocal);
                 mMap.addMarker(m);
                 ubicacionPedido=latLng;
                 ubicacion.setText(ubicacionPedido.toString());
+
+                //polyline
+                /*
+                Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
+                        .clickable(true)
+                        .add(ubicacionLocal, latLng));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacionLocal, 2));
+
+                 */
             }
         });
 
@@ -156,6 +165,35 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 finish();
             }
         });
+
+        //ubicacion del local
+        /*
+         Random r = new Random();
+
+        // Una direccion aleatoria de 0 a 359 grados
+        int direccionRandomEnGrados = r.nextInt(360);
+
+        // Una distancia aleatoria de 100 a 1000 metros
+        int distanciaMinima = 100;
+        int distanciaMaxima = 1000;
+        int distanciaRandomEnMetros = r.nextInt(distanciaMaxima - distanciaMinima) + distanciaMinima;
+
+        LocationManager locMa = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locMa.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        ubicacionLocal = new LatLng(location.getLatitude(), location.getLongitude());
+
+        LatLng nuevaPosicion = SphericalUtil.computeOffset(
+                ubicacionLocal,
+                distanciaRandomEnMetros,
+                direccionRandomEnGrados
+        );
+        markerLocal = new MarkerOptions()
+                .position(nuevaPosicion)
+                .title("Ubicación del local")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        mMap.addMarker(markerLocal);
+        */
+
     }
 
     //Habilitar ir a la posicion actual
